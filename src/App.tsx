bd89163e7be47b0eaf13d6d6bd064ea50cb24dce
Jeffrey Flynn <jeffreyneilflynn@gmail.com>
2020-05-13 17:14:18 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export const App = () => {
+  return (
+    <div className="primary-container">
+      <div className="landing-page" />
+
+      <div className="coming-soon">Coming 2020</div>
+
+      <div className="footer">
+        <span>
+          Contact us:
+          <a className="email" href="mailto:info@royalbine.com">
+            info@RoyalBine.com
+          </a>
+        </span>
+        <h6>102 New Jersey Ave, Absecon, NJ 08201</h6>
+      </div>
+    </div>
+  );
+};
